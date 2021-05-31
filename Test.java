3b7287009667ddd97b9b176a7861c608a8cc602a
nanijeevan <84911374+nanijeevan@users.noreply.github.com>
2021-05-31 14:29:35 +0530

    uploaded java files

---
+++ Test.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Test {
+    
+    public static void display()
+    {
+        System.out.println("display");
+    }
+    
+    public static void display1()
+    {
+        System.out.println("display111");
+    }
+    public static void main(String[] args){
+        //Scanner scan=new Scanner(System.in);
+      // int n= scan.nextInt();
+        System.out.println("hai");
+        Test.display();
+
+    }
+
+    
+}
---
+++ xprate.java
@@ -0,0 +1,6 @@
+public class xprate{
+    public static void main(String[] args) {
+        System.out.println("this is jeevan");
+        
+    }
+}
